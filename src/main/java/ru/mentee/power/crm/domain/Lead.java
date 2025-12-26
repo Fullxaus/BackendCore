d97aa4ec1e054ab7b44261eca0f3283b85732e21
Dmitrii <dmitrii.vidru@yandex.ru>
2025-12-26 23:58:35 +0300

    Microsoft Windows [Version 10.0.26200.7623] (c) Microsoft Corporation. All rights reserved. Active code page: 65001 Active code page: 65001
    C:\projects\BackendCore\.git-rewrite\t>������������ ���������������������� ��������������,������ �������� id ���������������������� uuid
    
    C:\projects\BackendCore\.git-rewrite\t>

--- src/main/java/ru/mentee/power/crm/domain/Lead.java
+++ src/main/java/ru/mentee/power/crm/domain/Lead.java
@@ -1,13 +1,40 @@
 package ru.mentee.power.crm.domain;
 
+import java.util.UUID;
+
 public class Lead {
-    private String id;
+    private UUID id;
     private String email;
     private String phone;
     private String company;
     private String status;
 
-    public Lead(String id, String email, String phone, String company, String status) {
+    /**
+     * Конструктор для создания нового лида с генерацией случайного UUID.
+     *
+     * @param email   Электронный адрес лида.
+     * @param phone   Номер телефона лида.
+     * @param company Компания лида.
+     * @param status  Статус лида.
+     */
+    public Lead(String email, String phone, String company, String status) {
+        this.id = UUID.randomUUID();
+        this.email = email;
+        this.phone = phone;
+        this.company = company;
+        this.status = status;
+    }
+
+    /**
+     * Конструктор для создания лида с существующим UUID.
+     *
+     * @param id      Уникальный идентификатор лида.
+     * @param email   Электронный адрес лида.
+     * @param phone   Номер телефона лида.
+     * @param company Компания лида.
+     * @param status  Статус лида.
+     */
+    public Lead(UUID id, String email, String phone, String company, String status) {
         this.id = id;
         this.email = email;
         this.phone = phone;
@@ -15,7 +42,7 @@ public class Lead {
         this.status = status;
     }
 
-    public String getId() {
+    public UUID getId() {
         return id;
     }
 
@@ -38,7 +65,7 @@ public class Lead {
     @Override
     public String toString() {
         return "Lead{" +
-                "id='" + id + '\'' +
+                "id=" + id +
                 ", email='" + email + '\'' +
                 ", phone='" + phone + '\'' +
                 ", company='" + company + '\'' +
@@ -46,4 +73,3 @@ public class Lead {
                 '}';
     }
 }
-
