849a81810994ec29a0c0553b6d7b96a50415ca53
Dmitrii <dmitrii.vidru@yandex.ru>
2025-12-27 01:47:52 +0300

    Создал LeadRepository  добавил к нему тесты.

---
+++ src/main/java/ru/mentee/power/crm/model/Lead.java
@@ -0,0 +1,33 @@
+package ru.mentee.power.crm.model;
+
+import java.util.UUID;
+
+public record Lead(
+        UUID id,
+        String email,
+        String phone,
+        String company,
+        String status
+) {
+    // Кастомный конструктор с валидацией
+    public Lead(UUID id, String email, String phone, String company, String status) {
+        this.id = id;
+        this.email = email.isEmpty() ? null : email;
+        this.phone = phone.isEmpty() ? null : phone;
+        this.company = company.isEmpty() ? null : company;
+        this.status = status.isEmpty() ? null : status;
+
+        if (email == null || email.isEmpty()) {
+            throw new IllegalArgumentException("Email is required");
+        }
+        if (phone == null || phone.isEmpty()) {
+            throw new IllegalArgumentException("Phone is required");
+        }
+        if (company == null || company.isEmpty()) {
+            throw new IllegalArgumentException("Company is required");
+        }
+        if (status == null || status.isEmpty()) {
+            throw new IllegalArgumentException("Status is required");
+        }
+    }
+}
