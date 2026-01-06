4e1b9b4aadeb49682e92bdf4a616c13f79135307
Dmitrii <dmitrii.vidru@yandex.ru>
2026-01-06 11:25:38 +0300

    Microsoft Windows [Version 10.0.26200.7623] (c) Microsoft Corporation. All rights reserved. Active code page: 65001 Active code page: 65001
    C:\projects\BackendCore\.git-rewrite\t>������������ Execution failed for task ':generateJte'.
    
    C:\projects\BackendCore\.git-rewrite\t>       > Failed to notify dependency resolution listener.
    
    C:\projects\BackendCore\.git-rewrite\t>          > 'java.util.Set org.gradle.api.artifacts.LenientConfiguration.getArtifacts(org.gradle.api.specs.Spec)'
    
    C:\projects\BackendCore\.git-rewrite\t>          ���������������� �� �������������������������� build.gradle
    
    C:\projects\BackendCore\.git-rewrite\t>

---
+++ src/main/java/ru/mentee/power/crm/spring/Application.java
@@ -0,0 +1,10 @@
+package ru.mentee.power.crm.spring;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+}
