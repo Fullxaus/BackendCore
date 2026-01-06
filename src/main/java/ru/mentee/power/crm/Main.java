f36a7014793f6e604c611cf173768cfaf329a357
Dmitrii <dmitrii.vidru@yandex.ru>
2026-01-06 09:43:31 +0300

    Microsoft Windows [Version 10.0.26200.7623] (c) Microsoft Corporation. All rights reserved. Active code page: 65001 Active code page: 65001
    C:\projects\BackendCore\.git-rewrite\t>���������������� ������������ LeadListServlet
    
    C:\projects\BackendCore\.git-rewrite\t>

--- src/main/java/ru/mentee/power/crm/Main.java
+++ src/main/java/ru/mentee/power/crm/Main.java
@@ -1,56 +1,59 @@
 package ru.mentee.power.crm;
 
 import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
 import ru.mentee.power.crm.domain.Address;
 import ru.mentee.power.crm.model.LeadStatus;
 import ru.mentee.power.crm.repository.LeadRepository;
 import ru.mentee.power.crm.service.LeadService;
 import ru.mentee.power.crm.servlet.LeadListServlet;
-import jakarta.servlet.annotation.WebServlet;
+
 import java.io.File;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        // Создайте LeadRepository (InMemoryLeadRepository)
+        // Создайте LeadRepository и LeadService
         LeadRepository repository = new LeadRepository();
-
-        // Создайте LeadService через конструктор DI (передайте Repository)
         LeadService leadService = new LeadService(repository);
 
-        // Добавьте 5 тестовых лидов через leadService.addLead
+        // Добавьте 5 тестовых лидов
         leadService.addLead("test1@example.com", "Company1", LeadStatus.NEW, new Address("Moscow", "Suvorova", "123456"), "1234567890");
         leadService.addLead("test2@example.com", "Company2", LeadStatus.NEW, new Address("St.Petersburg", "Pushkinskaya", "987654"), "9876543210");
         leadService.addLead("test3@example.com", "Company3", LeadStatus.NEW, new Address("Kazan", "Kazanskaya", "111111"), "1111111111");
         leadService.addLead("test4@example.com", "Company4", LeadStatus.NEW, new Address("Novosibirsk", "Novosibirskaya", "222222"), "2222222222");
         leadService.addLead("test5@example.com", "Company5", LeadStatus.NEW, new Address("Ekaterinburg", "Lermontova", "333333"), "3333333333");
 
-        // Создайте Tomcat экземпляр через new Tomcat()
+        // Создаём Tomcat и явный Connector
         Tomcat tomcat = new Tomcat();
 
-        // Установите порт через tomcat.setPort(8080)
-        tomcat.setPort(8080);
+        Connector connector = new Connector();
+        connector.setPort(8080);
+        tomcat.getService().addConnector(connector);
+        tomcat.setConnector(connector);
 
-        // Создайте контекст приложения через tomcat.addContext("", new File(".").getAbsolutePath())
+        // Создаём контекст (укажите корректный путь, если нужно)
         Context context = tomcat.addContext("", new File(".").getAbsolutePath());
 
-        // Сохраните LeadService в ServletContext через context.getServletContext().setAttribute("leadService", leadService)
+        // Кладём LeadService в ServletContext
         context.getServletContext().setAttribute("leadService", leadService);
 
-        // Зарегистрируйте LeadListServlet через tomcat.addServlet(context, "LeadListServlet", new LeadListServlet())
+        // Регистрируем сервлет и маппинг
         tomcat.addServlet(context, "LeadListServlet", new LeadListServlet());
-
-        // Задайте URL маппинг через context.addServletMappingDecoded("/leads", "LeadListServlet")
         context.addServletMappingDecoded("/leads", "LeadListServlet");
 
-        // Запустите сервер через tomcat.start()
+        // Стартуем Tomcat
         tomcat.start();
 
-        // Заблокируйте main поток через tomcat.getServer().await() (чтобы сервер не завершился)
-        tomcat.getServer().await();
+        // Выводим информацию о коннекторах для отладки
+        System.out.println("tomcat started; connectors:");
+        Arrays.stream(tomcat.getService().findConnectors())
+                .forEach(c -> System.out.println(" connector: " + c.getProtocol() + " port=" + c.getPort() + " state=" + c.getState()));
+        System.out.println("about to await");
 
-        System.out.println("Tomcat started on port 8080");
-        System.out.println("Open http://localhost:8080/leads in browser");
+        // Блокируем main поток
+        tomcat.getServer().await();
     }
 }
