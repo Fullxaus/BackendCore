5549a970cfb06c5679761e4a36c966f50bb0c116
Dmitrii <dmitrii.vidru@yandex.ru>
2026-01-15 16:30:25 +0300

    Feature/bcore 15.1 (#19)
    * Создал LeadRepository  добавил к нему тесты.
    
    * Создал LeadRepository  добавил к нему тесты.
    
    * Создал LeadService  добавил к нему тесты.
    
    * Создал HelloCrmServer
    
    * Создал LeadListServlet
    
    * Создал LeadListServlet
    
    * Исправил проект LeadListServlet
    
    * Провел рефакторинг, внес шаблонизатор JTE + Tailwind CSS
    
    * Ошибка Execution failed for task ':generateJte'.
           > Failed to notify dependency resolution listener.
              > 'java.util.Set org.gradle.api.artifacts.LenientConfiguration.getArtifacts(org.gradle.api.specs.Spec)'
              Проблема с зависимостями build.gradle
    
    * Исправил зависимости Gradle в проекте bcore-14
    
    * Проект компилируется,но при переходе на http://localhost:8081/leads ошибка There was an unexpected error (type=Not Found, status=404).
    
    * Ошибка в проекте
    Error starting ApplicationContext. To display the condition evaluation report re-run your application with 'debug' enabled.
    2026-01-15T16:26:35.104+03:00 ERROR 18944 --- [mentee-power-crm] [           main] o.s.boot.SpringApplication               : Application run failed

--- src/main/java/ru/mentee/power/crm/Application.java
+++ src/main/java/ru/mentee/power/crm/Application.java
@@ -3,7 +3,9 @@ package ru.mentee.power.crm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class Application {
 
     public static void main(String[] args) {
