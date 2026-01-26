1dc6a4117be4c073231720887e0ab892ed47386d
Dmitrii <dmitrii.vidru@yandex.ru>
2026-01-26 18:13:45 +0300

    Microsoft Windows [Version 10.0.26200.7623] (c) Microsoft Corporation. All rights reserved. Active code page: 65001 Active code page: 65001
    C:\projects\BackendCore\.git-rewrite\t>�������� �������� StackComparisonTest,�������� ���������������� ��������������
    
    C:\projects\BackendCore\.git-rewrite\t>

--- src/main/java/ru/mentee/power/crm/spring/controller/LeadController.java
+++ src/main/java/ru/mentee/power/crm/spring/controller/LeadController.java
@@ -1,5 +1,6 @@
 package ru.mentee.power.crm.spring.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
