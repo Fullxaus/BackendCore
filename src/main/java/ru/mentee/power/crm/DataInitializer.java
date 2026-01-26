85004bdf6916fc50f107883f1b729fa9f0c58408
Dmitrii <dmitrii.vidru@yandex.ru>
2026-01-26 18:13:45 +0300

    Microsoft Windows [Version 10.0.26200.7623] (c) Microsoft Corporation. All rights reserved. Active code page: 65001 Active code page: 65001
    C:\projects\BackendCore\.git-rewrite\t>�������� �������� StackComparisonTest,�������� ���������������� ��������������
    
    C:\projects\BackendCore\.git-rewrite\t>

---
+++ src/main/java/ru/mentee/power/crm/DataInitializer.java
@@ -0,0 +1,26 @@
+package ru.mentee.power.crm;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import ru.mentee.power.crm.domain.Address;
+import ru.mentee.power.crm.model.LeadStatus;
+import ru.mentee.power.crm.service.LeadService;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    private final LeadService leadService;
+
+    public DataInitializer(LeadService leadService) {
+        this.leadService = leadService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        leadService.addLead("test1@example.com", "Company1", LeadStatus.NEW, new Address("Moscow", "Suvorova", "123456"), "1234567890");
+        leadService.addLead("test2@example.com", "Company2", LeadStatus.NEW, new Address("St.Petersburg", "Pushkinskaya", "987654"), "9876543210");
+        leadService.addLead("test3@example.com", "Company3", LeadStatus.NEW, new Address("Kazan", "Kazanskaya", "111111"), "1111111111");
+        leadService.addLead("test4@example.com", "Company4", LeadStatus.NEW, new Address("Novosibirsk", "Novosibirskaya", "222222"), "2222222222");
+        leadService.addLead("test5@example.com", "Company5", LeadStatus.NEW, new Address("Ekaterinburg", "Lermontova", "333333"), "3333333333");
+    }
+}
