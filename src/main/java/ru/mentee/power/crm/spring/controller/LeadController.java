0af18af884b8ac61370e967303d301ca6925300f
Dmitrii <dmitrii.vidru@yandex.ru>
2026-01-26 03:23:59 +0300

    Feature/bcore 15.3 (#21)
    * Создал LeadRepository  добавил к нему тесты.
    
    * Создал LeadRepository  добавил к нему тесты.
    
    * Создал LeadService  добавил к нему тесты.
    
    * Создал HelloCrmServer
    
    * Создал LeadListServlet
    
    * Создал LeadListServlet
    
    * Исправил проект LeadListServlet
    
    * Провел рефакторинг, внес шаблонизатор JTE + Tailwind CSS
    
    * Ошибка Execution failed for task ':generateJte'.
           > Failed to notify dependency resolution listener.
              > 'java.util.Set org.gradle.api.artifacts.LenientConfiguration.getArtifacts(org.gradle.api.specs.Spec)'
              Проблема с зависимостями build.gradle
    
    * Исправил зависимости Gradle в проекте bcore-14
    
    * Проект компилируется,но при переходе на http://localhost:8081/leads ошибка There was an unexpected error (type=Not Found, status=404).
    
    * Ошибка в проекте
    Error starting ApplicationContext. To display the condition evaluation report re-run your application with 'debug' enabled.
    2026-01-15T16:26:35.104+03:00 ERROR 18944 --- [mentee-power-crm] [           main] o.s.boot.SpringApplication               : Application run failed
    
    * Исправил в проекте list.jte
    Внес в него создание лида. Проект работает.
    
    * Исправил в проекте list.jte
    Внес в LeadController создание лида, PostMapping

--- src/main/java/ru/mentee/power/crm/spring/controller/LeadController.java
+++ src/main/java/ru/mentee/power/crm/spring/controller/LeadController.java
@@ -1,19 +1,36 @@
 package ru.mentee.power.crm.spring.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.mentee.power.crm.domain.Address;
 import ru.mentee.power.crm.model.Lead;
+import ru.mentee.power.crm.model.LeadStatus;
 import ru.mentee.power.crm.service.LeadService;
 import java.util.List;
 
 @Controller
 public class LeadController {
-
     private final LeadService leadService;
 
     public LeadController(LeadService leadService) {
         this.leadService = leadService;
+    }
+
+    @PostMapping("/lead")
+    public String createLead(
+                             @RequestParam String email,
+                             @RequestParam String company,
+                             @RequestParam LeadStatus status,
+                             @RequestParam String phone,
+                             @RequestParam String city,
+                             @RequestParam String street,
+                             @RequestParam String zip  ){
+        Address adress = new Address(city, street, zip);
+        leadService.addLead(email,company,status,adress,phone);
+
+        return "redirect:/leads";
     }
 
     @GetMapping("/leads")
