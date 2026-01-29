e014f6f3a86009c8ed174fc72a377f8f305d1cf3
Dmitrii <dmitrii.vidru@yandex.ru>
2026-01-30 01:25:09 +0300

    Исправил класс Application

--- src/main/java/ru/mentee/power/crm/Application.java
+++ src/main/java/ru/mentee/power/crm/Application.java
@@ -2,16 +2,8 @@ package ru.mentee.power.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-
 
 @SpringBootApplication
-@ComponentScan(
-        basePackages = "ru.mentee.power.crm",
-        excludeFilters = @ComponentScan.Filter(
-                type = FilterType.ASSIGNABLE_TYPE,
-                classes = ru.mentee.power.crm.spring.Application.class))
 public class Application {
 
     public static void main(String[] args) {
