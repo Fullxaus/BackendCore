6711553fde27f9c2f25297d5252e9d14915df155
Dmitrii <dmitrii.vidru@yandex.ru>
2026-01-30 02:03:21 +0300

    Исправил ошибки при компиляции

---
+++ src/main/java/ru/mentee/power/crm/spring/SpringCrmApplications.java
@@ -0,0 +1,14 @@
+package ru.mentee.power.crm.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class SpringCrmApplications {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCrmApplications.class, args);
+    }
+}
