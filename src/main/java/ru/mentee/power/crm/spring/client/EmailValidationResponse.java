39f88fadc7e79d388f7bdaa0120194da96791aa5
Dmitrii <dmitrii.vidru@yandex.ru>
2026-03-05 01:37:40 +0300

    Написал RestСontroller и WireMock интеграционный тест

--- src/main/java/ru/mentee/power/crm/spring/client/EmailValidationResponse.java
+++ src/main/java/ru/mentee/power/crm/spring/client/EmailValidationResponse.java
@@ -1,5 +1,3 @@
 package ru.mentee.power.crm.spring.client;
 
-public record EmailValidationResponse(String email, boolean valid, String reason) {
-
-}
+public record EmailValidationResponse(String email, boolean valid, String reason) {}
